eb24da5b8037b23df972f97bfa7672a489884536
Administrator <Administrator@WIN-20240201IRH>
2024-04-02 17:15:32 +0800

    更新

--- src/guessequation/view/View.java
+++ src/guessequation/view/View.java
@@ -8,54 +8,59 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class View {
+	public ArrayList<JPanel> ajp = new ArrayList<JPanel>();
+	public int index = 0;// 当前操作的数字位置
+	public int line = 0;// 当前操作的数字位置
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		// 创建一个JFrame窗口
+
+		ArrayList<JLabel> ajl = new ArrayList<JLabel>();
+		View view = new View();
 		JFrame frame = new JFrame("Button Example");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置窗口关闭时的操作
 		frame.setSize(800, 600); // 设置窗口大小
-		// 创建 FlowLayout 实例并设置水平和垂直间距  
-        FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 0);
-        JPanel panelts = new JPanel();
-		panelts.setBackground(Color.WHITE); 
+		// 创建 FlowLayout 实例并设置水平和垂直间距
+		FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 0);
+		JPanel panelts = new JPanel();
+		panelts.setBackground(Color.WHITE);
 		panelts.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 		// 创建一个JPanel面板，用于放置按钮
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);  
+		panel.setBackground(Color.WHITE);
 		frame.add(panel); // 将面板添加到窗口中
 		panel.add(panelts);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		Dimension dbutton = new Dimension(57, 57);
 		try {
 			// 加载图片
 			BufferedImage image = ImageIO.read(new File("src/resource/1.png")); // 替换为你的图片路径
 			ImageIcon icon = new ImageIcon(image);
 			JPanel panelt = new JPanel();
-			panelt.setBackground(Color.WHITE); 
+			panelt.setBackground(Color.WHITE);
 			panelt.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 			for (int j = 0; j < 6; j++) {
 				JPanel panel1 = new JPanel();
-				panel1.setBackground(Color.WHITE);  
+				panel1.setBackground(Color.WHITE);
 				panel1.setLayout(layout);
 				for (int i = 0; i < 7; i++) {
 					// 创建一个JLabel来显示图片
 					JLabel label = new JLabel(icon);
+					label.setPreferredSize(dbutton);
+					ajl.add(label);
 					panel1.add(label);
 				}
 				panel.add(panel1);
 			}
 			panel.add(panelt);
-		} catch (IOException e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(frame, "Error loading image: " + e.getMessage());
-		}
-		JPanel panel2 = new JPanel();
-		panel2.setBackground(Color.WHITE);  
-		panel2.setLayout(layout);
-		try {
-			Dimension dbutton = new Dimension(57, 57);
+			JPanel panel2 = new JPanel();
+			panel2.setBackground(Color.WHITE);
+			panel2.setLayout(layout);
+
 			// 加载自定义图片
 			ImageIcon icon1 = new ImageIcon(ImageIO.read(new File("src/resource/number1.png")));
 			// 创建一个按钮
@@ -70,7 +75,10 @@ public class View {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// 在点击按钮时显示一串字符
-					JOptionPane.showMessageDialog(frame, "Hello, World!");
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon1);
+					if (view.index < 6)
+						view.index++;
 				}
 			});
 			// 加载自定义图片
@@ -82,6 +90,17 @@ public class View {
 			button2.setBorderPainted(false); // 不绘制按钮边框
 			button2.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button2); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button2.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon2);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon3 = new ImageIcon(ImageIO.read(new File("src/resource/number3.png")));
 			// 创建一个按钮
@@ -91,6 +110,17 @@ public class View {
 			button3.setBorderPainted(false); // 不绘制按钮边框
 			button3.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button3); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button3.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon3);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon4 = new ImageIcon(ImageIO.read(new File("src/resource/number4.png")));
 			// 创建一个按钮
@@ -100,6 +130,17 @@ public class View {
 			button4.setBorderPainted(false); // 不绘制按钮边框
 			button4.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button4); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button4.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon4);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon5 = new ImageIcon(ImageIO.read(new File("src/resource/number5.png")));
 			// 创建一个按钮
@@ -109,6 +150,17 @@ public class View {
 			button5.setBorderPainted(false); // 不绘制按钮边框
 			button5.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button5); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button5.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon5);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon6 = new ImageIcon(ImageIO.read(new File("src/resource/number6.png")));
 			// 创建一个按钮
@@ -118,6 +170,17 @@ public class View {
 			button6.setBorderPainted(false); // 不绘制按钮边框
 			button6.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button6); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button6.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon6);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon7 = new ImageIcon(ImageIO.read(new File("src/resource/number7.png")));
 			// 创建一个按钮
@@ -127,6 +190,17 @@ public class View {
 			button7.setBorderPainted(false); // 不绘制按钮边框
 			button7.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button7); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button7.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon7);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon8 = new ImageIcon(ImageIO.read(new File("src/resource/number8.png")));
 			// 创建一个按钮
@@ -136,6 +210,17 @@ public class View {
 			button8.setBorderPainted(false); // 不绘制按钮边框
 			button8.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button8); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button8.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon8);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon9 = new ImageIcon(ImageIO.read(new File("src/resource/number9.png")));
 			// 创建一个按钮
@@ -145,6 +230,17 @@ public class View {
 			button9.setBorderPainted(false); // 不绘制按钮边框
 			button9.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button9); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button9.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon9);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
 			// 加载自定义图片
 			ImageIcon icon0 = new ImageIcon(ImageIO.read(new File("src/resource/number0.png")));
 			// 创建一个按钮
@@ -154,40 +250,71 @@ public class View {
 			button0.setBorderPainted(false); // 不绘制按钮边框
 			button0.setContentAreaFilled(false); // 不填充按钮内容区域背景
 			panel2.add(button0); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button0.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.line*7 + view.index).setIcon(icon0);
+					if (view.index < 6)
+						view.index++;
+				}
+			});
+
+			JPanel panel3 = new JPanel();
+			panel3.setBackground(Color.WHITE);
+			panel3.setLayout(layout);
+			// 创建一个按钮
+			JButton button11 = new JButton("<-");
+			panel3.add(button11); // 将按钮添加到面板中
+			// 为按钮添加点击事件监听器
+			button11.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					ajl.get(view.index).setIcon(icon);
+					if (view.index > 0)
+						view.index--;
+				}
+			});
+			// 创建一个按钮
+			JButton button12 = new JButton("+");
+			panel3.add(button12); // 将按钮添加到面板中
+			// 创建一个按钮
+			JButton button13 = new JButton("-");
+			panel3.add(button13); // 将按钮添加到面板中
+			// 创建一个按钮
+			JButton button14 = new JButton("x");
+			panel3.add(button14); // 将按钮添加到面板中
+			// 创建一个按钮
+			JButton button15 = new JButton("/");
+			panel3.add(button15); // 将按钮添加到面板中
+			// 创建一个按钮
+			JButton button16 = new JButton("=");
+			panel3.add(button16); // 将按钮添加到面板中
+			// 创建一个按钮
+			JButton button17 = new JButton("enter");
+			panel3.add(button17); // 将按钮添加到面板中
+			button17.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// 在点击按钮时显示一串字符
+					// JOptionPane.showMessageDialog(frame, "Hello, World!");
+					view.index = 0;
+					view.line++;
+				}
+			});
+
+			panel.add(panel2);
+			panel.add(panel3);
+			// 显示窗口
+			frame.setVisible(true);
 		} catch (IOException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(frame, "Error loading image: " + e.getMessage());
 		}
-
-		JPanel panel3 = new JPanel();
-		panel3.setBackground(Color.WHITE);  
-		panel3.setLayout(layout);
-		// 创建一个按钮
-		JButton button11 = new JButton("<-");
-		panel3.add(button11); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button12 = new JButton("+");
-		panel3.add(button12); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button13 = new JButton("-");
-		panel3.add(button13); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button14 = new JButton("x");
-		panel3.add(button14); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button15 = new JButton("/");
-		panel3.add(button15); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button16 = new JButton("=");
-		panel3.add(button16); // 将按钮添加到面板中
-		// 创建一个按钮
-		JButton button17 = new JButton("enter");
-		panel3.add(button17); // 将按钮添加到面板中
-
-		panel.add(panel2);
-		panel.add(panel3);
-		// 显示窗口
-		frame.setVisible(true);
 	}
 
 }
